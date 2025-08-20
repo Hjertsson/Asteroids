0185bb19c05d0f3eddc528f6b7e0a000e7342233
Hjertsson <gustav@hjertsson.se>
2025-08-20 09:45:13 +0200

    started adding asteroid framework

---
+++ asteroid.py
@@ -0,0 +1,16 @@
+from circleshape import CircleShape
+from constants import *
+import pygame
+
+class Asteroid(CircleShape):
+	 def __init__(self, x, y, radius):
+                
+	
+
+
+	def draw()
+		pygame.draw.circle(self.x, self.y,  self.radius, 2)
+		
+		pygame.draw.polygon(screen, "white", self.triangle(), 2)
+
+	def update()
